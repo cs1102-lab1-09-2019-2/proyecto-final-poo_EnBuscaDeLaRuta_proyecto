d303f75b1bda5ae80c6e10e5471c1ff21395e5f8
DavidCaleb <54277172+DavidCaleb@users.noreply.github.com>
2019-10-28 00:20:44 -0500

    Create implementacion.h

---
+++ implementacion.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <cmath>
+#include <random>
+using namespace std;
+
+#include "semaforo.h"
+#include "mapa.h"
+
+typedef int Entero;
